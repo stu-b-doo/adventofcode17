18253990a034b99f8d9d74f9ca662e1aa5f4b3b6
Stuart Bell <stu-b-doo@users.noreply.github.com>
2017-12-07 11:41:15 +0000

    add string split

--- 01_inversecaptcha.js
+++ 01_inversecaptcha.js
@@ -6,7 +6,9 @@ const repeated = (x, i, a) => equals(x, a[i+1]),
 filterRepeated = addIndex(filter)(repeated),
 // prepend the last element to test whether first and last digits are identical
 decircularize = a => prepend(last(a), identity(a)),
-inverseCaptcha = pipe(decircularize, filterRepeated, sum);
+// split string to array 
+spl = splitEvery(1),
+inverseCaptcha = pipe(spl, decircularize, filterRepeated, sum);
 
 // tests
 const testFn = fn => ([input, output]) => equals(output, applyTo(input, fn)),
@@ -15,8 +17,9 @@ applyTests = (fn, aTests) => filter(compose(not, testFn(fn)), aTests);
 
 // identify any test failures. Empty array is what we're going for
 applyTests(inverseCaptcha, [
-  [[9,1,2,1,2,1,2,9], 9],
-  [[1,1,1,1], 4],
-  [[1,2,3,4], 0],
-  [[1,1,2,2], 3]
+  ['91212129', 9],
+  ['1111', 4],
+  ['1234', 0],
+  ['1122', 3]
 ])
+
